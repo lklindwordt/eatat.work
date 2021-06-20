bcafd6f57e61b6993600f0993c8cf9384886c67a
Lars Klindwordt <info@cobarian.de>
2021-06-20 18:57:02 +0200

    changed build paths

--- wp-app/wp-content/themes/eatatwork/assets/webpack.config.js
+++ wp-app/wp-content/themes/eatatwork/assets/webpack.config.js
@@ -2,23 +2,24 @@
  * Webpack configuration.
  */
 
-const path = require( 'path ');
+const path = require( 'path' );
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
-// const OptimizeCssAssetsPlugin = require( 'optimize-css-assets-webpack-plugin' );
-// const cssnano = require( 'cssnano' ); // https://cssnano.co/
+const OptimizeCssAssetsPlugin = require( 'optimize-css-assets-webpack-plugin' );
+const cssnano = require( 'cssnano' );                                             // https://cssnano.co/
 const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
-// const UglifyJsPlugin = require( 'uglifyjs-webpack-plugin' );
-const CopyPlugin = require('copy-webpack-plugin'); // https://webpack.js.org/plugins/copy-webpack-plugin/
-const DependencyExtractionWebpackPlugin = require( '@wordpress/dependency-extraction-webpack-plugin' );
+const UglifyJsPlugin = require( 'uglifyjs-webpack-plugin' );
+// const CopyPlugin = require("copy-webpack-plugin");                                // https://webpack.js.org/plugins/copy-webpack-plugin/
+// const DependencyExtractionWebpackPlugin = require( '@wordpress/dependency-extraction-webpack-plugin' );
 
-const JS_DIR = path.resolve( __dirname, '/src/js' );
-const IMG_DIR = path.resolve( __dirname, '/src/img' );
-const BUILD_DIR = path.resolve( __dirname, '/build' );
+const JS_DIR = path.resolve( __dirname, 'src/js' );
+const IMG_DIR = path.resolve( __dirname, 'src/img' );
+const BUILD_DIR = path.resolve( __dirname, 'build' );
 
 const entry = {
   main: JS_DIR + '/main.js',
   single: JS_DIR + '/single.js',
 };
+
 const output = {
   path: BUILD_DIR,
   filename: 'js/[name].js'
@@ -60,11 +61,11 @@ const plugins = ( argv ) => [
 		filename: 'css/[name].css'
 	}),
 
-	new CopyPlugin({
-		patterns: [
-			{ from: LIB_DIR, to: BUILD_DIR + '/library' }
-		]
-	}),
+	// new CopyPlugin({
+	// 	patterns: [
+	// 		{ from: LIB_DIR, to: BUILD_DIR + '/library' }
+	// 	]
+	// }),
 
 	// new DependencyExtractionWebpackPlugin({
 	// 	injectPolyfill: true,
@@ -79,6 +80,19 @@ module.exports = ( env, argv ) => ({
   module: {
     rules: rules, 
   },
+  optimization: {
+		minimizer: [
+			new OptimizeCssAssetsPlugin({
+				cssProcessor: cssnano
+			}),
+
+			new UglifyJsPlugin( {
+				cache: false,
+				parallel: true,
+				sourceMap: false
+			})
+		]
+	},
   plugins: plugins( argv ), 
   externals: {
 		jquery: 'jQuery'
