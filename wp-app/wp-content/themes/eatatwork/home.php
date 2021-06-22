d15b59b4d9a8eb22fbb3f5e9e8ae4886f3bfe515
Lars Klindwordt <info@cobarian.de>
2021-06-22 06:13:17 +0200

    add rank math

--- wp-app/wp-content/themes/eatatwork/home.php
+++ wp-app/wp-content/themes/eatatwork/home.php
@@ -6,49 +6,44 @@
  */
 
   get_header();
-?>
 
-<div id="primary">
-  <main id="main" class="site-main mt-5" role="main">
-    <?php
-      if ( have_posts() ) :
-        ?>
-        <div class="container">
-          <?php
-            if ( is_home() && !is_front_page() ) {
-              ?>  
-              <div class="mb-5">
-                <h1 class="page-title screen-reader-text">
-                  <?php single_post_title(); ?>
-                </h1>
-              </div>
-              <?php
-            }
-          ?>
-          <div class="row">
-            <?php 
-              while ( have_posts() ) : the_post();
-                ?>
+  ?>
 
-                <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
-                  <?php get_template_part( 'partials/posts/preview' ); ?>
-                </div>
+<header class="entry-header">
+  <div class="entry-header-img mb-4">
+    <img width="1150" height="767" src="http://127.0.0.1/wp-content/uploads/2021/06/BKR_Truck_DWP2544-1152w.png" class="card-img-top attachment-featured-full size-featured-image" alt="" loading="lazy" sizes="(max-width: 1296px) 1296px, auto" srcset="http://127.0.0.1/wp-content/uploads/2021/06/BKR_Truck_DWP2544-1152w.png 1150w, http://127.0.0.1/wp-content/uploads/2021/06/BKR_Truck_DWP2544-1152w-300x200.png 300w, http://127.0.0.1/wp-content/uploads/2021/06/BKR_Truck_DWP2544-1152w-1024x683.png 1024w, http://127.0.0.1/wp-content/uploads/2021/06/BKR_Truck_DWP2544-1152w-768x512.png 768w">
+  </div>
+  <div class="container">
+    <h1 class="entry-title default-max-width"><?php echo single_post_title(); ?></h1>
+  </div>
 
-                <?php
-              endwhile;
+</header><!-- .entry-header -->
+
+<?php
+
+  if ( have_posts() ) :
+    
+    ?>
+    <div class="container">
+      <div class="row">
+        <?php 
+          while ( have_posts() ) : the_post();
             ?>
-          </div>
-        </div>
-        <?php
-      else :
-        get_template_part( 'partials/posts/none' );
-      endif;
 
-      eatatwork_pagination();
-     ?>
-  </main>
-</div>
-  
-<?php 
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+              <?php get_template_part( 'partials/posts/preview' ); ?>
+            </div>
+
+            <?php
+          endwhile;
+        ?>
+      </div>
+    </div>
+    <?php
+  else :
+    get_template_part( 'partials/posts/none' );
+  endif;
+
+  eatatwork_pagination();
   
   get_footer();
--- wp-app/wp-content/themes/eatatwork/page.php
+++ wp-app/wp-content/themes/eatatwork/page.php
@@ -6,28 +6,13 @@
  */
 
   get_header();
-?>
 
-<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-      
-			<?php
 
-			while ( have_posts() ) :
-				the_post();
+  while ( have_posts() ) :
+		the_post();
 
-				get_template_part( 'template-parts/content/content', 'page' );
+		get_template_part( 'template-parts/content/content', 'page' );
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
-
-			endwhile;
-			?>
-
-		</main>
-	</div>
-
-<?php 
+	endwhile;
+ 
   get_footer();
